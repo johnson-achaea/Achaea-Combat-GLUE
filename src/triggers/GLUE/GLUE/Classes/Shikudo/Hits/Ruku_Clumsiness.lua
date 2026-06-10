623031fe0829118cfce3666e6516c393050a0ca7
johnson-achaea <ajgarbagegamemail@gmail.com>
2026-06-10 15:28:13 -0600

    Wire Kuro/Ruku hits into aff queue; fix Shatter trigger script field

--- src/triggers/GLUE/GLUE/Classes/Shikudo/Hits/Ruku_Clumsiness.lua
+++ src/triggers/GLUE/GLUE/Classes/Shikudo/Hits/Ruku_Clumsiness.lua
@@ -1,22 +1,9 @@
---[[
-    GLUE Trigger: Ruku (Clumsiness / Healthleech)
-    Pattern: Staff cracks against arm
-
-    Behavior:
-    - Gives clumsiness first
-    - If clumsiness already present, gives healthleech instead
-    - At kata 10+, gives both (clumsiness then healthleech)
-]]--
-
 if not GLUE or not GLUE.IsTarget then return end
 if GLUE.illusionCheck() then return end
+if not GLUE.IsTarget(matches[2]) then return end
 
-local targetName = matches[2]
-if not GLUE.IsTarget(targetName) then return end
-
--- Ruku gives clumsiness first, then healthleech if clumsiness already present
-GLUE.state.AddSmartAffliction({"clumsiness", "healthleech"})
-
-if shik and shik.kata and shik.kata > 10 then
-    GLUE.state.AddAffliction("healthleech")
-end
+local kataBonus = shik and shik.kata and shik.kata > 10
+GLUE.affQueue.Queue(function()
+    GLUE.state.AddSmartAffliction({"clumsiness", "healthleech"})
+    if kataBonus then GLUE.state.AddAffliction("healthleech") end
+end)
